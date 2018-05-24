bddd685a251dc15a1918c32cf4946978a0f4910d
Jeremy Leng <jleng@bcap.com>
2018-05-24 07:22:21 -0700

    updates for deployment

--- dist/js/aws.js
+++ dist/js/aws.js
@@ -1,2 +1,4 @@
 //https://wubeplw72h.execute-api.us-east-1.amazonaws.com
-var proxyHost = 'wubeplw72h.execute-api.us-east-1.amazonaws.com';
+//wubeplw72h.execute-api.us-east-1.amazonaws.com - dev
+//fmrkicyqqg.execute-api.us-east-1.amazonaws.com - staging
+var proxyHost = 'fmrkicyqqg.execute-api.us-east-1.amazonaws.com';
